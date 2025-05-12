9132e2e9ba6a33fb9b249e8e4ec7b71751e733d2
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-12 07:28:29 +0530

    positive num

---
+++ positive_num.java
@@ -0,0 +1,12 @@
+public class positive_num {
+    public static void main(String[] args) {
+        int num = -1;  
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+        // This is a simple program to check if a number is positive
+        // or not. It uses an if-else statement to determine the result.
+    }
+}
