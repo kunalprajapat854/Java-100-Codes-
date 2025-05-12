387b61fbd0bbc3d1676cb9e7026cf2cb687c6c99
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-12 08:42:35 +0530

    even-odd code with multiple solutions

---
+++ even_odd.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class even_odd {
+     public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        int num =  scanner.nextInt();
+      
+         System.out.println("Enter the number " + num);
+
+      //   if(num % 2 ==0){
+      //     System.out.println("The given number is even " + "= "+ num);
+      //   } else {
+      //    System.out.println("The given number is odd "  +"= "+ num );
+      //   }
+
+      // using ternary operator 
+      // String even = (num %2==0) ? "Even" : "odd" ;
+      // System.out.println(even);
+
+      int n =    num % 2 ;
+       switch (n) {
+         case 0:
+            System.out.println("Even");
+            break;
+
+         case 1:
+            System.out.println("Odd");
+       
+         default:
+            break;
+       }
+
+       
+     }
+   } 
+
+
