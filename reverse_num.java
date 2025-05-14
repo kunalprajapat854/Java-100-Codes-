ccf804946ef80b21f4df215df86a977b779954b3
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-14 18:02:10 +0530

    reverse number with while loop

---
+++ reverse_num.java
@@ -0,0 +1,13 @@
+public class reverse_num {
+    public static void main(String[] args) {
+        int num = 123, remainder = 0, reverse = 0;
+
+        while (num != 0) {
+            remainder = num % 10;
+            reverse = reverse * 10 + remainder;
+            num /= 10;
+        }
+        System.out.println("Reversed Num " + reverse);
+    }
+
+}
