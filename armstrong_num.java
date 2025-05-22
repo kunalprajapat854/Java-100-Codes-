1bf5bca4ff36b71cc1dd498fa422721cd76090d0
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-23 01:04:59 +0530

    armstrong number code

---
+++ armstrong_num.java
@@ -0,0 +1,25 @@
+public class armstrong_num {
+    public static void main(String[] args) {
+        int number = 3712, count = 0, sum = 0, temp;
+        temp = number;
+        // Step 1 count the number of digits
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+        // Step 2 calculate the digit ^n and add to the sum
+        temp = number;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, count);
+            temp /= 10;
+        }
+        // step 3 compare the sum and the original number
+        if (number == sum) {
+            System.out.println(number + " is a armstrong number");
+        } else {
+            System.out.println(number + " is not a armstrong number");
+        }
+
+    }
+}
