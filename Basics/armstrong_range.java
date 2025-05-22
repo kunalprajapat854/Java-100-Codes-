5df70bc8523480a924a0a76436a8f6bd3508047f
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-23 01:53:02 +0530

    armstrong range 1 to 10000

--- Basics/armstrong_range.java
+++ Basics/armstrong_range.java
@@ -2,12 +2,46 @@ package Basics;
 
 public class armstrong_range {
     public static void main(String[] args) {
-        int count = 0, digit = 0;
-        for (int number = 1; number <= 10000; number++) {
-            digit = number % 10;
-            count++;
-        }
-        System.out.println(count);
-    }
 
+        // output
+        // 1
+        // 2
+        // 3
+        // 4
+        // 5
+        // 6
+        // 7
+        // 8
+        // 9
+        // 153
+        // 370
+        // 371
+        // 407
+        // 1634
+        // 8208
+        // 9474
+
+        for (int number = 1; number <= 10000; number++) {
+            int temp = number;
+            int count = 0, digit = 0, sum = 0;
+
+            while (temp != 0) {
+                temp /= 10;
+                count++;
+            }
+
+            temp = number;
+            while (temp != 0) {
+
+                digit = temp % 10;
+                sum += Math.pow(digit, count);
+                temp /= 10;
+            }
+
+            if (sum == number) {
+                System.out.println(number);
+            }
+
+        }
+    }
 }
