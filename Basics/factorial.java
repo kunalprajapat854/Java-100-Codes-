40ed5e6372cfe669cea248b310363e177232904d
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-05-24 10:22:17 +0530

    factorial code

---
+++ Basics/factorial.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println("factorial of " + num + " is : " + fact);
+    }
+
+}
