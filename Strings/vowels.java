581be12df289a649b2913496ebd6c9ee78adff00
kunalprajapat854 <kunalprajapat221@gmail.com>
2025-08-16 11:29:36 +0530

    vowels string code

---
+++ Strings/vowels.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class vowels {
+    public static void main(String[] args) {
+        char c = 'a';
+
+        if (vowels(c)) {
+            System.out.println(c + " is a vowel");
+        } else {
+            System.out.println(c + " is constant");
+        }
+    }
+
+    static boolean vowels(char c) {
+
+        c = Character.toUpperCase(c);
+        return (c == 'A' || c == 'I' || c == 'O' || c == 'E' || c == 'U');
+    }
+
+}
